c309e77253fdd8a3202348bc3eafc53d5aedbb95
tsoudibi <a220284268@gmail.com>
2021-08-11 16:57:00 +0800

    add threading on mediapipe, fix lag

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame.camera
 import cv2
 import mediapipe as mp
 import time
+import threading
 
 # intilize
 pygame.init()
@@ -19,28 +20,36 @@ mp_pose = mp.solutions.pose
 camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-ret, background = camera.read()
+
+# take first image 
+_, background = camera.read()
+background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
+background = cv2.flip(background.swapaxes(0, 1), 0) # swap axis and flip image
 
 # create screen 
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('pygame with mediapipe')
 
-# image backup regard of video capture fail
+# cap images, contain main image and backup regard of video capture fail
+image_main = background
 image_backup = background
+
+# bool to kill looping
+loop_pose = True
 
 # funtion to run mediapipe and create image 
 def mediapipe_pose():
-    global image_backup
+    global image_main, image_backup, camera
     with mp_pose.Pose(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as pose:
-        global camera
-        if camera.isOpened():
+        while  camera.isOpened() and loop_pose:
+            time1 = time.time()
             success, image = camera.read()
             if not success:
                 print("Ignoring empty camera frame.")
                 # video capture fail, use backup image
-                return image_backup
+                image_main = image_backup
 
             # Flip the image horizontally for a later selfie-view display, and convert
             # the BGR image to RGB.
@@ -49,40 +58,53 @@ def mediapipe_pose():
             # pass by reference.
             image.flags.writeable = False
             results = pose.process(image)
-
             # Draw the pose annotation on the image.
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             mp_drawing.draw_landmarks( 
                 image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
             # after drawing, change color back tp rgb
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             # backup image 
             image_backup = image
-            #return to pygame
-            return image
-        return image_backup
+            # save image to global
+            image_main = image
+            print('success' + str(time.time()-time1))
+        # if anything wrong happend, use backup image
+        image_main = image_backup
         # https://google.github.io/mediapipe/solutions/pose#static_image_mode
 
-# run the game
-running = True
-while running:
-    # event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# create a thread to run mediapipe, than start it 
+t = threading.Thread(target = mediapipe_pose)
+t.start()
 
-    # status update
-    # set bakground as video capture, and resize as window size if not 
-    background = mediapipe_pose()
-    background = cv2.flip(background.swapaxes(0, 1), 0) # swap axis and flip image
-    pygame.surfarray.blit_array(screen, background)
+def main():
+    global image_main
+    # run the game
+    running = True
+    while running:
+        # event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # status update
+        # set bakground as video capture, and resize as window size if not 
+        background = image_main
+        background = cv2.flip(background.swapaxes(0, 1), 0) # swap axis and flip image
+        pygame.surfarray.blit_array(screen, background)
 
 
-    # screen drawing
-    pygame.display.update()
+        # screen drawing
+        pygame.display.update()
 
-pygame.quit()
-cv2.destroyAllWindows()
+    # kill the thread 
+    global loop_pose
+    loop_pose = False
+    t.join()
 
+    pygame.quit()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
